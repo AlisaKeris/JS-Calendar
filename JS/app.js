96124cca30f1b2bea9b3ce3be1af3c050e30e506
Opilane TTHK <opilane@tthk.ee>
2020-12-07 11:57:02 +0200

    JS  Advent Calendar

---
+++ JS/app.js
@@ -0,0 +1,24 @@
+const button = document.querySelector('.btn-start');
+const container = document.querySelector('.container');
+const days = 25;
+const openDoor=(path,event)=>{
+    event.target.parentNode.style.backgroundImage = `url("${path}")`;
+    event.target.style.backgroundColor = "#333";
+    event.target.style.opacity = 0;
+}
+const createCalendar = ()=>{
+    for (let i =0;i<days;i++){
+        const calendarDoor = document.createElement('div');
+        const calendarDoorText = document.createElement('div');
+        calendarDoor.classList.add('image');
+        calendarDoor.style.gridArea = "door" + (i +1);
+        container.appendChild(calendarDoor);
+        calendarDoorText.classList.add('text');
+        calendarDoorText.innerHTML = i +1;
+        calendarDoor.appendChild(calendarDoorText); 
+        let picNumber = i +1;
+        let picPath = `./img/pic${picNumber}.jpg`;
+        calendarDoorText.addEventListener('click',openDoor.bind(null,picPath));
+    }
+}
+button.addEventListener('click', createCalendar);
